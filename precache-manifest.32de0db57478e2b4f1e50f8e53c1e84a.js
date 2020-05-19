34d70c738ce20d1e969a442e27180b56682d9f54
dkrypt <Deepak.Kumar91@ge.com>
2020-05-20 03:55:33 +0530

    Updates

---
+++ precache-manifest.32de0db57478e2b4f1e50f8e53c1e84a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db7cfa9379adb12038a581f9428e8701",
+    "url": "/ReactToDo/index.html"
+  },
+  {
+    "revision": "d51b3b8ec18e0a6953e4",
+    "url": "/ReactToDo/static/css/main.0790ec8e.chunk.css"
+  },
+  {
+    "revision": "8015dbd4c66c3537d9e0",
+    "url": "/ReactToDo/static/js/2.cd2f80f5.chunk.js"
+  },
+  {
+    "revision": "fbdb25c6a30511ca0525a561a8e4c009",
+    "url": "/ReactToDo/static/js/2.cd2f80f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51b3b8ec18e0a6953e4",
+    "url": "/ReactToDo/static/js/main.59e682f0.chunk.js"
+  },
+  {
+    "revision": "e5cb0285ad55624d7d06",
+    "url": "/ReactToDo/static/js/runtime-main.b96dbb46.js"
+  }
+]);
